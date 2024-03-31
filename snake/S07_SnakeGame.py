2f00ff63fcc220d39158810a90510828259bcba9
dariakatkova <108279926+dariakatkova@users.noreply.github.com>
2024-04-01 00:39:47 +0200

    Update S07_SnakeGame.py

--- snake/S07_SnakeGame.py
+++ snake/S07_SnakeGame.py
@@ -43,13 +43,13 @@ class Snake(Animal):
     """
     def __init__(self):
         Animal.__init__(self, "🟩", 3)
-        self.body_size = 1
+        self.body_size = 4
         self.squares = []
-        self.coordinates = []
+        self.coordinates = [] #contains tuple (number of block, x, y)
         self.letter_repr = 'S'
 
         for i in range(0, self.body_size):
-            self.coordinates.append([0, 0])
+            self.coordinates.append([0, 0]) #nb_block, cell_number
             self.squares.append(i)
 
         #self.turn()
@@ -101,8 +101,6 @@ class SnakeGame(PlanetTk): #mechanics of the game
     __AUTHORISED_TYPES = {Ground, Snake}
     def __init__(self,root, lines_count, columns_count, cell_size=20, obstacles=False):
         PlanetTk.__init__(self, root, "Snake Game", lines_count, columns_count, {Ground, Snake, Food, Obstacle}, cell_size=cell_size)
-        cell_number = self.get_cell_number_from_coordinates((lines_count-1)//2, (columns_count-1) //2)
-        #PlanetTk._born(self, cell_number, Snake)
 
         self.obstacles = obstacles
         self.gamestatus = 1
@@ -116,6 +114,7 @@ class SnakeGame(PlanetTk): #mechanics of the game
         self.__lines_count = lines_count
         self.__columns_count = columns_count
         self.coordinate_head = 0
+        self.coordinate_tail = 0
 
 
     def pause(self):
@@ -149,7 +148,13 @@ class SnakeGame(PlanetTk): #mechanics of the game
     def restart(self):
         center = (self.__lines_count // 2-1, self.__columns_count// 2-1)
         self.coordinate_head = self.get_cell_number_from_coordinates(center[0], center[1])
+        self.coordinate_tail = self.get_cell_number_from_coordinates(center[0], center[1])
         PlanetTk._born(self, self.coordinate_head, Snake)
+        for i in range(self.snake.body_size - 1):
+            cell_number = self.get_cell_number_from_coordinates(center[0], center[1] - i - 1)
+            PlanetTk._born(self, cell_number, Snake)
+            self.snake.coordinates.append([i, cell_number])
+        self.coordinate_tail = self.get_cell_number_from_coordinates(center[0], center[1])
         self.gamestatus = 1
         self.snake.alive()
 
@@ -185,7 +190,7 @@ class SnakeGameWindow(tk.Toplevel):
 
     def back_to_menu(self):
         pass
-    
+
 class MyApp(tk.Tk):
 
     def __init__(self):
