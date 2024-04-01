eb35097dfa7d7f9b1976859800ecb575bea728d6
dariakatkova <108279926+dariakatkova@users.noreply.github.com>
2024-04-01 19:03:53 +0200

    Update S07_SnakeGame.py

--- snake/S07_SnakeGame.py
+++ snake/S07_SnakeGame.py
@@ -3,6 +3,7 @@ from S07_TP14_01 import *
 from S07_TP15 import PlanetTk
 import keyboard
 import time
+import os
 
 class Food(Resource):
     """
@@ -33,17 +34,11 @@ class Snake(Animal):
     def __init__(self):
         Animal.__init__(self, "🟩", 3)
         self.body_size = 4
-        self.squares = []
-        self.coordinates = [] #contains list [number of block, cell number]
+        self.coordinates = [] #contains list [number of block, i, j]
         self.letter_repr = 'S'
 
-        for i in range(0, self.body_size):
-            self.coordinates.append([0, 0]) #nb_block, cell_number
-            self.squares.append(i)
 
-        #self.turn()
-
-    def turn(self):  #running with sudo failed because python3 not configured for tkinter
+    def turn_left(self):  #running with sudo failed because python3 not configured for tkinter
         while True:
             if keyboard.read_key() == "left":
                 direction = []
@@ -56,34 +51,20 @@ class Snake(Animal):
                 else: #snake going right
                     direction = [1, 1]
                 self.set_current_direction(direction[0], direction[1])
-
-                break
-
-            elif keyboard.read_key() == "right":
-                if self.get_current_direction() == [1, 1]:  # snake going up
-                    direction = [1, 0]
-                elif self.get_current_direction() == [0, 0]:  # snake going down
-                    direction = [-1, 0]
-                elif self.get_current_direction() == [-1, 0]:  # snake going left
-                    direction = [1, 1]
-                else:  # snake going right
-                    direction = [0, 0]
+    def turn_right(self):
+        if keyboard.read_key() == "right":
+            if self.get_current_direction() == [1, 1]:  # snake going up
+                direction = [1, 0]
+            elif self.get_current_direction() == [0, 0]:  # snake going down
+                direction = [-1, 0]
+            elif self.get_current_direction() == [-1, 0]:  # snake going left
+                direction = [1, 1]
+            else:  # snake going right
+                direction = [0, 0]
                 self.set_current_direction(direction[0], direction[1])
-
-                break
-
-    def death(self):
-        """
-        if the snake collides with an object, with itself, with the border of the grid
-        """
-        self.char_repr = "🟧"
 
     def alive(self):
         self.char_repr = "🟩"
-
-    def growth(self):
-        self.body_size += 1
-        #self.coordinates.append()
 
 
 class SnakeGame(PlanetTk): #mechanics of the game
@@ -95,7 +76,6 @@ class SnakeGame(PlanetTk): #mechanics of the game
         self.gamestatus = 1
         self.score = 0
         self.life = True
-        self.lives_left = 3
         self.speed = 15
         self.snake = Snake()
         self.paused = True
@@ -114,10 +94,13 @@ class SnakeGame(PlanetTk): #mechanics of the game
             self.paused = False
 
     def move(self):
-        #self.snake.turn()
         while self.life == True:
             if self.paused == False:
-                
+                current_coordinates_head = self.snake.coordinates[0][1:]
+                old_coordinates = self.coordinate_tail
+                for i in range(len(self.snake.coordinates)):
+                    if 
+
 
                 time.sleep(0.5)
 
@@ -125,15 +108,15 @@ class SnakeGame(PlanetTk): #mechanics of the game
         center = (self.__lines_count // 2-1, self.__columns_count// 2-1)
         self.coordinate_head = self.get_cell_number_from_coordinates(center[0], center[1])
         self.coordinate_tail = self.get_cell_number_from_coordinates(center[0], center[1])
-        PlanetTk._born(self, self.coordinate_head, Snake)
-        for i in range(self.snake.body_size - 1):
-            cell_number = self.get_cell_number_from_coordinates(center[0], center[1] - i - 1)
+        for i in range(self.snake.body_size):
+            cell_number = self.get_cell_number_from_coordinates(center[0], center[1]-i)
             PlanetTk._born(self, cell_number, Snake)
-            self.snake.coordinates.append([i, cell_number])
+            self.snake.coordinates.append([i, center[0], center[1]- i])
         self.coordinate_tail = self.get_cell_number_from_coordinates(center[0], center[1])
         self.gamestatus = 1
         self.snake.alive()
         self.add_obstacles()
+        print(self.snake.coordinates)
 
     def game_over(self):
         if self.gamestatus == 0:
@@ -144,10 +127,12 @@ class SnakeGame(PlanetTk): #mechanics of the game
         while self.gamestatus == 1:
             #if self.paused == False:
                 #self.move()
+                #self.turn()
+                self.step()
                 self.generate_apple()
                 self.eats_apples()
                 print(PlanetTk.__str__(self))
-                print("\033", end="")
+                os.system('clear')
                 time.sleep(0.5)
 
     def add_obstacles(self):
@@ -165,54 +150,29 @@ class SnakeGame(PlanetTk): #mechanics of the game
             PlanetTk.die(self, food_position, Food) #or food()?
             self.snake.body_size += 1
             self.score += 1
+            i, j = self.get_coordinates_from_cell_number(food_position)
+            self.snake.coordinates.append([self.snake.body_size, i, j])
+            return True
+
+    def step(self):
+        if self.eats_apples() == True:
+           pass
 
     def win(self):
-        if self.score > 50:
-            #add label "you won", pause snake
-            pass
+        if self.score > 10:
+             self.paused = True
+            #add label
 
 
     def collisions_obst(self):
         if 'Obstacle' in PlanetTk.get_classes_cell_numbers(self): #check if obstacles exist
             list_of_obstacles = PlanetTk.get_classes_cell_numbers(self)['Obstacle']
             if self.coordinate_head in list_of_obstacles:
-                self.lives_left -= 1
-                if self.lives_left == 0:
-                    self.life = False
-                    self.game_over()
-
-
-class SnakeGameWindow(tk.Toplevel):
-    def __init__(self, master, **kw):
-        tk.Toplevel.__init__(self, master, **kw)
-        self.__master = master
-        self.__game = SnakeGame(lines_count=30, columns_count=50, cell_size=20)
-        self.__game.pack()
-        tk.Button(self, text="Quit", command=self.destroy).pack()
-        tk.Button(self, text="Pause", command=self.pause).pack()
-        tk.Button(self, text="Back to menu", command=self.pause).pack()
-        self.title("Snake Game")
-
-    def pause(self):
-        pass
-
-    def back_to_menu(self):
-        pass
-
-class MyApp(tk.Tk):
-
-    def __init__(self):
-        tk.Tk.__init__(self)
-        self.title("Some Planets")
-        self.geometry("200x200")
-        tk.Button(self, text='Conway game', command=lambda: ConwayWindow(self)).pack(side=tk.TOP)
-        tk.Button(self, text='Turmites game', command=lambda: TurmitesWindow(self)).pack(side=tk.TOP)
-        tk.Button(self, text='Snake game', command=lambda: SnakeGameWindow(self)).pack(side=tk.TOP)
-        tk.Button(self, text='Quit', command=self.quit).pack(side=tk.TOP)
+                self.life = False
+                self.game_over()
 
 
 if __name__ == "__main__":
-    #MyApp().mainloop()
     ROOT = tk.Tk()
     GAME = SnakeGame(ROOT, 10, 10, 10, True)
     GAME.start_game()
